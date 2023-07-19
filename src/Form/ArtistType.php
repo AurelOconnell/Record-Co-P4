ad2b8a95634744a57477d5ca28e62653a46d89bc
AurelOconnell <aurelienoconnell@gmail.com>
2023-07-19 17:22:03 +0200

    adding labels in ArtistForm

--- src/Form/ArtistType.php
+++ src/Form/ArtistType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Artist;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,15 @@ class ArtistType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('city')
-            ->add('description')
+            ->add('name', TextType::class, [
+                'label' => 'Nom Artiste/Groupe'
+            ])
+            ->add('city', TextType::class, [
+                'label' => 'Ville origine',
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Rapide description'
+            ])
         ;
     }
 
