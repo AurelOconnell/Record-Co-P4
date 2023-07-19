1f728e72ee789ce5ca9c7edbd589dd555990e8e2
AurelOconnell <aurelienoconnell@gmail.com>
2023-07-19 16:41:28 +0200

    crud for record

---
+++ src/Controller/RecordController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Record;
+use App\Form\RecordType;
+use App\Repository\RecordRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/record')]
+class RecordController extends AbstractController
+{
+    #[Route('/', name: 'app_record_index', methods: ['GET'])]
+    public function index(RecordRepository $recordRepository): Response
+    {
+        return $this->render('record/index.html.twig', [
+            'records' => $recordRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_record_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, RecordRepository $recordRepository): Response
+    {
+        $record = new Record();
+        $form = $this->createForm(RecordType::class, $record);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recordRepository->save($record, true);
+
+            return $this->redirectToRoute('app_record_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('record/new.html.twig', [
+            'record' => $record,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_record_show', methods: ['GET'])]
+    public function show(Record $record): Response
+    {
+        return $this->render('record/show.html.twig', [
+            'record' => $record,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_record_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Record $record, RecordRepository $recordRepository): Response
+    {
+        $form = $this->createForm(RecordType::class, $record);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recordRepository->save($record, true);
+
+            return $this->redirectToRoute('app_record_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('record/edit.html.twig', [
+            'record' => $record,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_record_delete', methods: ['POST'])]
+    public function delete(Request $request, Record $record, RecordRepository $recordRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $record->getId(), $request->request->get('_token'))) {
+            $recordRepository->remove($record, true);
+        }
+
+        return $this->redirectToRoute('app_record_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
