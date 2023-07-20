8b509d29f0330c541ea20437051d8e2b6b44b715
AurelOconnell <aurelienoconnell@gmail.com>
2023-07-20 15:49:35 +0200

    artist crud working + more elements on the map

--- assets/app.js
+++ assets/app.js
@@ -22,40 +22,50 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
     maxZoom: 20
 }).addTo(myMap);
 
-var artists = {
-    "Led Zeppelin": { "lat": 51.50685034958066, "lon": -0.12563124287429342 },
-    "Kyuss": { "lat": 33.832684271178856, "lon": -116.54060105263638 },
+var artistsData = {
+    "11": {"name": "Led Zeppelin",  "lat": 51.50685034958066, "lon": -0.12563124287429342 },
+    "12": {"name": "Kyuss", "lat": 33.832684271178856, "lon": -116.54060105263638 },
+    "6": {"name": "Nirvana", "lat": 47.60562166337458, "lon": -122.32695829876475 },
+    "7": {"name": "Noir Désir", "lat": 44.83788806747539, "lon": -0.5777751973353323 },
+    "9": {"name": "The Beatles", "lat": 53.406041018478284, "lon": -2.982041514635397 },
 }
 
-for (var artist in artists) {
-    var marker = L.marker([artists[artist].lat, artists[artist].lon])
+for (var artistId in artistsData) {
+    var artist = artistsData[artistId];
+    var link = "<a href='/artist/" + encodeURIComponent(artistId) + "'>" + artist.name + "</a>";
+    var marker = L.marker([artist.lat, artist.lon])
         .addTo(myMap);
-    marker.bindPopup("<h4>" + artist + "</h4>");
+    marker.bindPopup("<h4>" + link + "</h4>");
 }
 
-// var marker = L.marker([51.50685034958066, -0.12563124287429342]).addTo(myMap);
-// marker.bindPopup("<h5>Led Zeppelin</h5>")
+function getCoordinates(cityName, artistName) {
+    $.ajax({
+        url: '{{ path("app_geo_coord") }}',
+        method: 'GET',
+        data: { city: cityName },
+        success: function (response) {
+            var coordinates = JSON.parse(response);
+            if (coordinates && coordinates.lat && coordinates.lon) {
+                artistsName[artistName] = { lat: coordinates.lat, lon: coordinates.lon };
+                var marker = L.marker([coordinates.lat, coordinates.lon]).addTo(myMap);
+                marker.bindPopup("<h4>" + artistName + "</h4>");
+            } else {
+                alert("Impossible de récupérer les coordonnées pour la ville spécifiée.");
+            }
+        },
+        error: function () {
+            alert("Une erreur s'est produite lors de la récupération des coordonnées.");
+        }
+    });
+}
 
-// var marker = L.marker([33.832684271178856, -116.54060105263638]).addTo(myMap);
-// marker.bindPopup("<h5>Kyuss</h5>")
+$("#addArtistButton").on('click', function () {
+    var cityName = $("artist_city").val();
+    var artistName = $("artist_name").val();
 
-// var iconVinyl = L.icon({
-//     iconUrl: "/assets/images/icon_vinyl.png",
-//     iconSize: [35, 35],
-//     iconAnchor: [12.5, 25],
-//     popupAnchor: [0, 0],
-// });
-
-// var breweries = {
-//     "Nomade Brewery": { "lat": 45.73126983642578, "lon": 4.985140800476074 },
-//     "La Débauche": { "lat": 45.65844509244459, "lon": 0.1680949586376146 },
-//     "Zoobrew": { "lat": 43.64225769042969, "lon": 3.9191091060638428 },
-//     "Piggy Brewing Compagny": { "lat": 48.763998, "lon": 6.059797 },
-//     "L'Agrivoise": { "lat": 45.0158629610363, "lon": 4.386628185462709 },
-//     "Brasserie Iron": { "lat": 43.975976224756494, "lon": 1.4139738535551438 },
-// }
-
-// for (brewery in breweries) {
-//     var marker = L.marker([breweries[brewery].lat, breweries[brewery].lon], { icon: iconVinyl }).addTo(myMap);
-//     marker.bindPopup("<h4>" + brewery + "</h4>");
-// }
+    if (cityName && artistName) {
+        getCoordinates(cityName, artistName);
+    } else {
+        alert("Veuillez saisir le nom de l'artiste et la ville d'origine.");
+    }
+});
